7959a00af0d0ccb6e8d571b120ab0cf733f73107
burn3dx <bektemir.jalankuzov8899@gmail.com>
2026-03-26 11:29:11 +0500

    Time: 0 ms (100.00%) | Memory: 21 MB (60.60%) - LeetSync

---
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def check(node, low, high):
+            if node is None:
+                return True
+
+            if not (low < node.val < high):
+                return False
+
+            return check(node.left, low, node.val) and check(node.right, node.val, high)
+
+        return check(root, float('-inf'), float('inf'))        
