26ad98301f82a2cd400c697cb5d52df6c03865c8
burn3dx <bektemir.jalankuzov8899@gmail.com>
2026-04-22 00:47:24 +0500

    Time: 523 ms (56.28%) | Memory: 19.7 MB (55.92%) - LeetSync

---
+++ 322-coin-change/coin-change.py
@@ -0,0 +1,13 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+            # создаём массив, заполненный "бесконечностью"
+        dp = [float('inf')] * (amount + 1)
+        dp[0] = 0
+
+        for i in range(1, amount + 1):
+            for coin in coins:
+                if i - coin >= 0:
+                    dp[i] = min(dp[i], dp[i - coin] + 1)
+
+        # если невозможно собрать сумму
+        return dp[amount] if dp[amount] != float('inf') else -1
